f1c1f6485a2908180ed6a2deccdcfea98ca45006
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-01-01 16:01:41 -0800

    hide and show form on button click

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+$(() => {
+  $('#add-form').hide();
+
+  $('.select-book').on('click', e => {
+    e.preventDefault();
+    e.stopPropagation();
+    $('main').hide();
+    $('#add-form').show();
+  })
+
+  $('#add-form').on('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('gottem');
+  });
+});
